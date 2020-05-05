83e470cdfeb8d8345a60bceb599ae9975ef0854e
Art-Im <jazzdm34@gmail.com>
2020-05-05 09:52:34 +0900

    change gulpfile.js & install cssmin & install rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
-gulp.task('hello', function(done) {
+gulp.task('hello', function (done) {
     console.log('Привет Мир!');
     done();
 });
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// minimize file css
+gulp.task('default', function () {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
